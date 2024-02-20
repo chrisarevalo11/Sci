060b8e7bb380900a319cdd7f0eef017268a7abd3
Christian Arevalo <christian.arevalo.fonseca@gmail.com>
2024-02-20 00:39:44 -0500

    :recycle: Renamed route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from '@/components/home/Home'
 import Layout from '@/components/layout/Layout'
 import Projects from '@/components/projects/Projects'
+import Tokens from '@/components/tokens/Tokens'
 
 function App() {
 	return (
@@ -11,8 +12,8 @@ function App() {
 				<Route path='/' element={<Home />} />
 				<Route path='/app' element={<Layout />}>
 					<Route path='projects' element={<Projects />} />
-					<Route path='tokens' element={<h1>Tokens</h1>} />
-					<Route path='dashboard' element={<h1>Dashboard</h1>} />
+					<Route path='tokens' element={<Tokens />} />
+					<Route path='new-round' element={<h1>Dashboard</h1>} />
 				</Route>
 			</Routes>
 		</HashRouter>
