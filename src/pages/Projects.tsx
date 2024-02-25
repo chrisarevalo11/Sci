f7353647b51bb47069caf0875bcb11641ab30ff6
Christian Arevalo <christian.arevalo.fonseca@gmail.com>
2024-02-24 19:19:03 -0500

    :sparkles: Implemented projects sections

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,6 +3,8 @@ import { ethers } from 'ethers'
 import { Link } from 'react-router-dom'
 import { useAccount } from 'wagmi'
 
+import ProjectCard from '@/components/projects/ProjectCard'
+import { Skeleton } from '@/components/ui/skeleton'
 import { getContracts } from '@/helpers/contracts'
 import { roundsApiFirebase } from '@/middlewares/firebase/round.firebase.middleware'
 import { Project } from '@/models/project.model'
@@ -12,7 +14,8 @@ import { GAS_LIMIT, SCI_ADMIN_ADDRESS } from '@/utils/variables/constants'
 export default function Projects(): JSX.Element {
 	const { getLastRound, updateRound } = roundsApiFirebase()
 
-	const { alloContract, qVSimpleStrategyContract } = getContracts()
+	const { allo: alloContract, qVSimpleStrategy: qVSimpleStrategyContract } =
+		getContracts()
 
 	const { address } = useAccount()
 
@@ -42,6 +45,7 @@ export default function Projects(): JSX.Element {
 	const onDistribute = async () => {
 		try {
 			setLoading(true)
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			const ethereum = (window as any).ethereum
 
 			if (!ethereum) {
@@ -93,32 +97,60 @@ export default function Projects(): JSX.Element {
 		;(async () => {
 			await getStates()
 		})()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 	return (
-		<div>
-			<h1>Projects</h1>
-			{loading ? (
-				<h1>Loading...</h1>
-			) : (
-				<div>
-					{projects.map((project: Project) => (
-						<div key={project.name}>
-							<h2>{project.name}</h2>
-							<p>{project.description}</p>
-							<Link
-								to={`/app/projects/${project.recipientId}`}
-								state={{ round, project }}
-							>
-								Details
-							</Link>
-						</div>
-					))}
-					{address === SCI_ADMIN_ADDRESS && (
-						<button onClick={onDistribute}>distribute</button>
-					)}
-				</div>
-			)}
-		</div>
+		<section className='w-full h-[max(100%, fit-content)] p-4 md:p-10 relative md:overflow-hidden'>
+			<header className='flex justify-end pb-3 border-b-4 items-center border-customBlack border-dashed'>
+				<h2>Projects</h2>
+			</header>
+			<div className='grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-5 mt-10 justify-items-center'>
+				{loading ? (
+					Array.from({ length: 12 }).map((_, index) => (
+						<Skeleton
+							key={index}
+							className='!w-[290px] !h-[300px] rounded-tl-xl rounded-br-xl'
+						/>
+					))
+				) : (
+					<>
+						{projects.map((project: Project) => (
+							<div key={project.name}>
+								<h2>{project.name}</h2>
+								<p>{project.description}</p>
+								<Link
+									to={`/app/projects/${project.recipientId}`}
+									state={{ round, project }}
+								>
+									Details
+								</Link>
+							</div>
+						))}
+						{Array.from({ length: 12 }).map((_, index) => (
+							<ProjectCard round={round} key={index} project={projectTest} />
+						))}
+						{address === SCI_ADMIN_ADDRESS && (
+							<button onClick={onDistribute}>distribute</button>
+						)}
+					</>
+				)}
+			</div>
+		</section>
 	)
+}
+
+const projectTest: Project = {
+	banner:
+		'https://www.brightidea.com/wp-content/uploads/Who_Participates_in_a_Hackathon.png',
+	description:
+		'This is a mock description for a mock project that I created for testing purposes of the Sci protocol',
+	github: 'https://github.com',
+	logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png',
+	name: 'DeSci Colombia',
+	recipientId: '0x7753E5f36f20B14fFb6b6a61319Eb66f63abdb0b',
+	slogan: 'This is a slogan',
+	tags: [],
+	twitter: '@comoes',
+	website: 'https://www.lego.com'
 }
