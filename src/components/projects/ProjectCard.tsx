54f5d34129ee60d5f0706ae91cab9e34cf723037
Christian Arevalo <christian.arevalo.fonseca@gmail.com>
2024-02-24 19:17:25 -0500

    :iphone: Worked on responsive and general styles

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,11 +1,40 @@
+import { Link } from 'react-router-dom'
+
+import { Project } from '@/models/project.model'
+import { Round } from '@/models/round.model'
+
 type Props = {
-	title: string
+	project: Project
+	round: Round | null
 }
 
-export default function ProjectCard(): JSX.Element {
+export default function ProjectCard(props: Props): JSX.Element {
+	const { project, round } = props
 	return (
-		<div>
-			<img src='' alt='' />
+		<div className='rounded-tl-3xl rounded-br-3xl !w-[290px] !h-[300px] flex flex-col overflow-hidden'>
+			<div className='h-1/3 w-full relative'>
+				<img
+					src={project.banner}
+					alt={project.name}
+					className='h-full w-full object-cover'
+				/>
+				<img
+					src={project.logo}
+					alt={project.name}
+					className='absolute size-10 left-5 -bottom-5'
+				/>
+			</div>
+			<div className='flex flex-col gap-4 p-4 bg-customBlack text-customWhite'>
+				<h4 className='text-right'>{project.name}</h4>
+				<p>{project.description}</p>
+				<Link
+					className='btn mx-auto btn-green'
+					to={`/app/projects/${project.recipientId}`}
+					state={{ round, project }}
+				>
+					See more
+				</Link>
+			</div>
 		</div>
 	)
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -30,7 +30,7 @@ export default function Dashboard(): JSX.Element {
 	}, [address])
 
 	return (
-		<section className='w-full h-full p-4 md:p-10 relative'>
+		<section className='w-full h-[max(100%, fit-content)] p-4 md:p-10 relative'>
 			<header className='flex justify-end pb-3 border-b-4 items-center border-customBlack border-dashed'>
 				<h2>Dashboard</h2>
 			</header>
@@ -44,7 +44,7 @@ export default function Dashboard(): JSX.Element {
 			<img
 				src='/images/slime-no-bg.webp'
 				alt='slime'
-				className='absolute rotate-45 -z-10 right-[-20px] top-[100px] '
+				className='absolute rotate-90 -z-10 left-[-20px] top-[100px] '
 			/>
 		</section>
 	)
