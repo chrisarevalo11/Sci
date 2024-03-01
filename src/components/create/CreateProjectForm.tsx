7154ffd3b98a23cfb93b56e11429096383e3905c
Christian Arevalo <christian.arevalo.fonseca@gmail.com>
2024-03-01 09:50:18 -0500

    :sparkles: Added create project form

--- src/components/create/CreateProjectForm.tsx
+++ src/components/create/CreateProjectForm.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { BytesLike, ethers, ZeroAddress } from 'ethers'
 import { useForm } from 'react-hook-form'
+import { useAccount } from 'wagmi'
 import { z } from 'zod'
 
 import {
@@ -10,71 +12,253 @@ import {
 	FormLabel,
 	FormMessage
 } from '@/components/ui/form'
+import { Textarea } from '@/components/ui/textarea'
+import { getContracts } from '@/helpers/contracts'
+import { reviewRecipients } from '@/helpers/relay'
+import { roundsApiFirebase } from '@/middlewares/firebase/round.firebase.middleware'
+import { Project } from '@/models/project.model'
+import { RecipientData } from '@/models/recipient-data.model'
+import { Round } from '@/models/round.model'
+import { toAbiCoder } from '@/utils'
+import {
+	GAS_LIMIT,
+	RECIPIENT_DATA_STRUCT_TYPES
+} from '@/utils/variables/constants'
+import { Status } from '@/utils/variables/enums'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const formSchema = z.object({
 	name: z.string().min(1, { message: 'Name is required' }),
+	slogan: z.string().min(1, { message: 'Slogan is required' }),
+	description: z.string().min(1, { message: 'Description is required' }),
 	banner: z.string().min(1, { message: 'Banner is required' }),
-	amount: z.string().min(1, { message: 'Amount is required' }),
-	registrationDeadline: z
+	logo: z.string().min(1, { message: 'Logo is required' }),
+	twitter: z
 		.string()
-		.min(1, { message: 'Registration deadline is required' }),
-	allocationDeadline: z
+		.min(1, { message: 'Twitter is required' })
+		.refine(value => !/\s/.test(value), {
+			message: 'Twitter cannot contain spaces'
+		}),
+	github: z.string().url({ message: 'It should be a url' }),
+	website: z
 		.string()
-		.min(1, { message: 'Allocation deadline is required' })
+		.min(1, { message: 'Website is required' })
+		.url({ message: 'Website should be a url' }),
+	tags: z.string().refine(
+		value => {
+			const regex = /^(\w+(,\s*\w+)*)?$/
+			const tagsArray = value.split(',').map(tag => tag.trim())
+			return regex.test(value) && tagsArray.length <= 5
+		},
+		{
+			message: 'Tags must be word(s) separated by commas and max. 5 tags'
+		}
+	)
 })
 
 export default function CreateProjectForm(): JSX.Element {
+	const { address } = useAccount()
+
 	const [loading, setLoading] = useState<boolean>(false)
+	const [round, setRound] = useState<Round | null>(null)
+
+	const { getLastRound, updateRound } = roundsApiFirebase()
+	const { allo } = getContracts()
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		defaultValues: {
 			name: '',
+			description: '',
+			slogan: '',
+			tags: '',
+			github: '',
+			twitter: '',
+			website: '',
 			banner: '',
-			amount: '',
-			registrationDeadline: '',
-			allocationDeadline: ''
+			logo: ''
 		},
 		resolver: zodResolver(formSchema)
 	})
 
-	const createProject = async (data: z.infer<typeof formSchema>) => {
-		setLoading(true)
-		console.log(data)
+	const getStates = async () => {
+		try {
+			setLoading(true)
+			const lastRound: Round = await getLastRound()
+			setRound(lastRound)
+			setLoading(false)
+		} catch (error) {
+			alert('Error: Look at console')
+			console.error(error)
+			setLoading(false)
+		}
 	}
 
+	const createProject = async (data: z.infer<typeof formSchema>) => {
+		console.log('data: ', data)
+		try {
+			setLoading(true)
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const ethereum = (window as any).ethereum
+
+			if (!ethereum) {
+				alert('Ethereum object not found')
+				return
+			}
+
+			const web3Provider: ethers.BrowserProvider = new ethers.BrowserProvider(
+				ethereum
+			)
+			await web3Provider.send('eth_requestAccounts', [])
+			const web3Signer: ethers.JsonRpcSigner = await web3Provider.getSigner()
+
+			const banner: string =
+				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2Fk6HOlMRHUM3ru78xLEFnmUyIwr4%3D%2F0x0%3A800x533%2F1200x800%2Ffilters%3Afocal(0x0%3A800x533)%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_image%2Fimage%2F30291399%2F800px-thoreaus_quote_near_his_cabin_site__walden_pond.0.jpg&f=1&nofb=1&ipt=8409d0f745c896681625cd32b0f36396463f6cc2b81643dbae6e639ba24b1b2f&ipo=images'
+			const description: string =
+				'Interpolation of field information with satellite information'
+			const github: string = 'www.github.com/recipients'
+			const logo: string =
+				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fep01.epimg.net%2Fcultura%2Fimagenes%2F2016%2F09%2F09%2Fbabelia%2F1473420066_993651_1473430939_noticia_normal.jpg&f=1&nofb=1&ipt=65fb6ff7f54bb2df9ed64412dac43ca6f3c9a1921e591cc4eb66e84165793eac&ipo=images'
+			const name: string = 'Recipients'
+			const slogan: string = 'Gaia is our home'
+			const tags: string[] = ['satellite', 'field']
+			const twitter: string = 'www.twitter.com/recipients'
+			const website: string = 'www.recipients.com'
+
+			if (!address) return
+
+			const recipientDataObject: RecipientData = {
+				recipientId: address,
+				recipientAddress: ZeroAddress,
+				metadata: {
+					protocol: BigInt(1),
+					pointer: 'ipfs://QmQmQmQmQmQmQmQmQmQmQmQmQm'
+				}
+			}
+
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const recipientDataArray: any[] = [
+				recipientDataObject.recipientId,
+				recipientDataObject.recipientAddress,
+				[
+					recipientDataObject.metadata.protocol,
+					recipientDataObject.metadata.pointer
+				]
+			]
+
+			const recipientData: BytesLike = toAbiCoder(
+				RECIPIENT_DATA_STRUCT_TYPES,
+				recipientDataArray
+			)
+
+			if (!round) return
+			if (!address) return
+
+			const registerRecipientTx = await allo
+				.connect(web3Signer)
+				.registerRecipient(round.poolId, recipientData, { gasLimit: GAS_LIMIT })
+
+			await registerRecipientTx.wait()
+
+			const reviewRecipientsResponse: string = await reviewRecipients(
+				round.address,
+				[address],
+				[Status.InReview]
+			)
+
+			console.log('reviewRecipientsResponse: ', reviewRecipientsResponse)
+
+			const project: Project = {
+				banner,
+				description,
+				github,
+				logo,
+				name,
+				recipientId: address,
+				slogan,
+				tags,
+				twitter,
+				website
+			}
+
+			if (!round.projects) {
+				round.projects = []
+			}
+
+			round.projects.push(project)
+			console.log('round', round)
+			await updateRound(round)
+
+			setLoading(false)
+		} catch (error) {
+			console.error(error)
+			alert('Error: Look at console')
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		if (address) {
+			;(async () => {
+				await getStates()
+			})()
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
 	return (
-		<div className='rounded-3xl border-2 border-customBlack gap-5 items-center flex flex-col bg-customWhite md:min-w-[400px] max-w-[500px] p-3 text-center'>
-			<h4>New Round</h4>
+		<div className='rounded-3xl border-2 mt-10 mx-auto border-customBlack gap-5 items-center flex flex-col bg-customWhite md:min-w-[400px] w-fit p-3 lg:p-5 text-center'>
+			<h4>New Project</h4>
 
 			<Form {...form}>
 				<form
 					onSubmit={form.handleSubmit(createProject)}
 					className='space-y-4 flex flex-col items-center'
 				>
-					<div className='flex flex-col items-start w-full'>
-						<FormLabel className='mr-2 font-bold mb-2'>Profile Id</FormLabel>
-						<FormControl>
-							<input
-								disabled
-								type='text'
-								className='w-full opacity-70'
-								placeholder='Test'
-							/>
-						</FormControl>
-					</div>
 					<FormField
 						control={form.control}
 						name='name'
 						render={({ field }) => (
 							<FormItem className='flex flex-col items-start w-full'>
-								<FormLabel className='mr-2 font-bold'>Round Name</FormLabel>
+								<FormLabel className='mr-2 font-bold'>Project Name</FormLabel>
 								<FormControl>
 									<input
-										type='text'
 										className='w-full'
-										placeholder='My round'
+										placeholder='My project'
 										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='slogan'
+						render={({ field }) => (
+							<FormItem className='flex flex-col items-start w-full'>
+								<FormLabel className='mr-2 font-bold'>Slogan</FormLabel>
+								<FormControl>
+									<input
+										{...field}
+										className='w-full'
+										placeholder='Promoting open science'
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='description'
+						render={({ field }) => (
+							<FormItem className='flex flex-col items-start w-full'>
+								<FormLabel className='mr-2 font-bold'>Description</FormLabel>
+								<FormControl>
+									<Textarea
+										{...field}
+										placeholder='This project is focused on...'
+										className='w-full'
 									/>
 								</FormControl>
 								<FormMessage />
@@ -88,25 +272,11 @@ export default function CreateProjectForm(): JSX.Element {
 							<FormItem className='flex flex-col items-start w-full'>
 								<FormLabel className='mr-2 font-bold'>Banner</FormLabel>
 								<FormControl>
-									<input type='file' className='w-full' {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name='amount'
-						render={({ field }) => (
-							<FormItem className='flex flex-col items-start w-full'>
-								<FormLabel className='mr-2 font-bold'>
-									Initial amount (DAI)
-								</FormLabel>
-								<FormControl>
 									<input
-										type='number'
+										type='file'
+										accept='image/*'
+										autoSave='true'
 										className='w-full'
-										placeholder='1880'
 										{...field}
 									/>
 								</FormControl>
@@ -116,12 +286,17 @@ export default function CreateProjectForm(): JSX.Element {
 					/>
 					<FormField
 						control={form.control}
-						name='registrationDeadline'
+						name='logo'
 						render={({ field }) => (
 							<FormItem className='flex flex-col items-start w-full'>
-								<FormLabel className='mr-2 font-bold'>Round Name</FormLabel>
+								<FormLabel className='mr-2 font-bold'>Logo</FormLabel>
 								<FormControl>
-									<input type='datetime-local' className='w-full' {...field} />
+									<input
+										type='file'
+										accept='image/*'
+										className='w-full'
+										{...field}
+									/>
 								</FormControl>
 								<FormMessage />
 							</FormItem>
@@ -129,23 +304,85 @@ export default function CreateProjectForm(): JSX.Element {
 					/>
 					<FormField
 						control={form.control}
-						name='allocationDeadline'
+						name='twitter'
 						render={({ field }) => (
 							<FormItem className='flex flex-col items-start w-full'>
-								<FormLabel className='mr-2 font-bold'>Round Name</FormLabel>
+								<FormLabel className='mr-2 font-bold'>
+									Twitter (without @)
+								</FormLabel>
 								<FormControl>
-									<input type='datetime-local' className='w-full' {...field} />
+									<input
+										{...field}
+										className='w-full'
+										placeholder='myproject'
+									/>
 								</FormControl>
 								<FormMessage />
 							</FormItem>
 						)}
 					/>
+					<FormField
+						control={form.control}
+						name='github'
+						render={({ field }) => (
+							<FormItem className='flex flex-col items-start w-full'>
+								<FormLabel className='mr-2 font-bold'>Github url</FormLabel>
+								<FormControl>
+									<input
+										{...field}
+										className='w-full'
+										placeholder='https://github.com/myproject'
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name='website'
+						render={({ field }) => (
+							<FormItem className='flex flex-col items-start w-full'>
+								<FormLabel className='mr-2 font-bold'>
+									Website/Linktree
+								</FormLabel>
+								<FormControl>
+									<input
+										{...field}
+										className='w-full'
+										placeholder='https://myproject.com'
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name='tags'
+						render={({ field }) => (
+							<FormItem className='flex flex-col items-start w-full'>
+								<FormLabel className='mr-2 font-bold'>Tags</FormLabel>
+								<FormControl>
+									<input
+										{...field}
+										className='w-full'
+										placeholder='tech, biology, etc.'
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
 					<button
-						className='btn btn-green !mt-5'
 						type='submit'
+						className='btn btn-green !mt-5'
 						disabled={loading}
 					>
-						{loading ? 'Loading...' : 'Create Round'}
+						{loading ? 'Loading...' : 'Create Project'}
 					</button>
 				</form>
 			</Form>
--- src/pages/CreateProject.tsx
+++ src/pages/CreateProject.tsx
@@ -1,180 +1,19 @@
-import { useEffect, useState } from 'react'
-import { BytesLike, ethers, ZeroAddress } from 'ethers'
-import { useNavigate } from 'react-router-dom'
-import { useAccount } from 'wagmi'
-
-import { getContracts } from '@/helpers/contracts'
-import { reviewRecipients } from '@/helpers/relay'
-import { roundsApiFirebase } from '@/middlewares/firebase/round.firebase.middleware'
-import { Project } from '@/models/project.model'
-import { RecipientData } from '@/models/recipient-data.model'
-import { Round } from '@/models/round.model'
-import { toAbiCoder } from '@/utils'
-import {
-	GAS_LIMIT,
-	RECIPIENT_DATA_STRUCT_TYPES
-} from '@/utils/variables/constants'
-import { Status } from '@/utils/variables/enums'
+import CreateProjectForm from '@/components/create/CreateProjectForm'
 
 export default function CreateProject(): JSX.Element {
-	const { address } = useAccount()
-	const { getLastRound, updateRound } = roundsApiFirebase()
-
-	const { allo: alloContract } = getContracts()
-
-	const navigate = useNavigate()
-
-	const [loading, setLoading] = useState<boolean>(true)
-	const [round, setRound] = useState<Round | null>(null)
-	const [syncronized, setSyncronized] = useState<boolean>(false)
-
-	const getStates = async () => {
-		try {
-			const lastRound: Round = await getLastRound()
-			setRound(lastRound)
-
-			setSyncronized(true)
-			setLoading(false)
-		} catch (error) {
-			alert('Error: Look at console')
-			console.error(error)
-			setLoading(false)
-		}
-	}
-
-	const onRegisterRecipient = async () => {
-		try {
-			setLoading(true)
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			const ethereum = (window as any).ethereum
-
-			if (!ethereum) {
-				alert('Ethereum object not found')
-				return
-			}
-
-			const web3Provider: ethers.BrowserProvider = new ethers.BrowserProvider(
-				ethereum
-			)
-			await web3Provider.send('eth_requestAccounts', [])
-			const web3Signer: ethers.JsonRpcSigner = await web3Provider.getSigner()
-
-			const banner: string =
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2Fk6HOlMRHUM3ru78xLEFnmUyIwr4%3D%2F0x0%3A800x533%2F1200x800%2Ffilters%3Afocal(0x0%3A800x533)%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_image%2Fimage%2F30291399%2F800px-thoreaus_quote_near_his_cabin_site__walden_pond.0.jpg&f=1&nofb=1&ipt=8409d0f745c896681625cd32b0f36396463f6cc2b81643dbae6e639ba24b1b2f&ipo=images'
-			const description: string =
-				'Interpolation of field information with satellite information'
-			const github: string = 'www.github.com/recipients'
-			const logo: string =
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fep01.epimg.net%2Fcultura%2Fimagenes%2F2016%2F09%2F09%2Fbabelia%2F1473420066_993651_1473430939_noticia_normal.jpg&f=1&nofb=1&ipt=65fb6ff7f54bb2df9ed64412dac43ca6f3c9a1921e591cc4eb66e84165793eac&ipo=images'
-			const name: string = 'Recipients'
-			const slogan: string = 'Gaia is our home'
-			const tags: string[] = ['satellite', 'field']
-			const twitter: string = 'www.twitter.com/recipients'
-			const website: string = 'www.recipients.com'
-
-			if (!address) return
-
-			const recipientDataObject: RecipientData = {
-				recipientId: address,
-				recipientAddress: ZeroAddress,
-				metadata: {
-					protocol: BigInt(1),
-					pointer: 'ipfs://QmQmQmQmQmQmQmQmQmQmQmQmQm'
-				}
-			}
-
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			const recipientDataArray: any[] = [
-				recipientDataObject.recipientId,
-				recipientDataObject.recipientAddress,
-				[
-					recipientDataObject.metadata.protocol,
-					recipientDataObject.metadata.pointer
-				]
-			]
-
-			const recipientData: BytesLike = toAbiCoder(
-				RECIPIENT_DATA_STRUCT_TYPES,
-				recipientDataArray
-			)
-
-			if (!round) return
-			if (!address) return
-
-			const registerRecipientTx = await alloContract
-				.connect(web3Signer)
-				.registerRecipient(round.poolId, recipientData, { gasLimit: GAS_LIMIT })
-
-			await registerRecipientTx.wait()
-
-			const reviewRecipientsResponse: string = await reviewRecipients(
-				round.address,
-				[address],
-				[Status.InReview]
-			)
-
-			console.log('reviewRecipientsResponse: ', reviewRecipientsResponse)
-
-			const project: Project = {
-				banner,
-				description,
-				github,
-				logo,
-				name,
-				recipientId: address,
-				slogan,
-				tags,
-				twitter,
-				website
-			}
-
-			if (!round.projects) {
-				round.projects = []
-			}
-
-			round.projects.push(project)
-			console.log('round', round)
-			await updateRound(round)
-
-			setLoading(false)
-		} catch (error) {
-			console.error(error)
-			alert('Error: Look at console')
-			setLoading(false)
-		}
-	}
-
-	useEffect(() => {
-		if (address) {
-			;(async () => {
-				await getStates()
-			})()
-		} else {
-			navigate('/app/projects')
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [address, syncronized])
-
 	return (
 		<section className='w-full h-[max(100%, fit-content)] p-4 md:p-10 relative'>
-			{loading ? (
-				<h1>Loading...</h1>
-			) : (
-				<>
-					<header className='flex justify-end pb-3 border-b-4 items-center border-customBlack border-dashed'>
-						<h2>Create Project</h2>
-					</header>
+			<header className='flex justify-end pb-3 border-b-4 items-center border-customBlack border-dashed'>
+				<h2>Create Project</h2>
+			</header>
 
-					<img
-						src='/images/slime-no-bg.webp'
-						alt='slime'
-						className='absolute rotate-180 -z-10 right-[-20px] top-[180px] '
-					/>
-					<div>
-						<button onClick={onRegisterRecipient}>registerRecipient</button>
-					</div>
-				</>
-			)}
+			<CreateProjectForm />
+
+			<img
+				src='/images/slime-no-bg.webp'
+				alt='slime'
+				className='fixed rotate-180 -z-10 right-[-20px] top-[180px] '
+			/>
 		</section>
 	)
 }
