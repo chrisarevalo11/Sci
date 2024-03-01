29fcb3a844ad63804243a6147501cf33266fe0e3
Christian Arevalo <christian.arevalo.fonseca@gmail.com>
2024-03-01 09:57:35 -0500

    :heavy_plus_sign: Added toast container and hook from shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,15 @@ import { useAccount } from 'wagmi'
 
 import Home from '@/components/home/Home'
 import Layout from '@/components/layout/Layout'
+import { Toaster } from '@/components/ui/toaster'
 import CreateProject from '@/pages/CreateProject'
 import Dashboard from '@/pages/Dashboard'
 import Faucet from '@/pages/Faucet'
 import Projects from '@/pages/Projects'
+import { AppDispatch } from '@/store'
 
 import ProjectComponent from './pages/Project'
 import { destroyERC20Details } from './store/slides/erc20Details.slice'
-import { AppDispatch } from './store'
 
 function App() {
 	const { isDisconnected } = useAccount()
@@ -22,21 +23,28 @@ function App() {
 		if (isDisconnected) {
 			dispatch(destroyERC20Details(''))
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isDisconnected])
 
 	return (
-		<HashRouter>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/app' element={<Layout />}>
-					<Route path='create-project' element={<CreateProject />} />
-					<Route path='dashboard' element={<Dashboard />} />
-					<Route path='faucet' element={<Faucet />} />
-					<Route path='projects' element={<Projects />} />
-					<Route path='projects/:recipientId' element={<ProjectComponent />} />
-				</Route>
-			</Routes>
-		</HashRouter>
+		<>
+			<HashRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/app' element={<Layout />}>
+						<Route path='create-project' element={<CreateProject />} />
+						<Route path='dashboard' element={<Dashboard />} />
+						<Route path='faucet' element={<Faucet />} />
+						<Route path='projects' element={<Projects />} />
+						<Route
+							path='projects/:recipientId'
+							element={<ProjectComponent />}
+						/>
+					</Route>
+				</Routes>
+			</HashRouter>
+			<Toaster />
+		</>
 	)
 }
 
